a99ed365ea1ffb2f7f52782db80cd661fc7bcb6c
Wony-Lee <a1_11_11@naver.com>
2021-06-25 00:36:43 +0900

    자연수 뒤집어 배열로 만들기, 제일 작은 수 제거하기

--- al/al2.js
+++ al/al2.js
@@ -33,21 +33,46 @@ function solution3(s) {
         })
         .join(" ");
 }
-
 solution3("try hello world");
 
+// 정수 내림차순 정렬
 function solution4(n) {
     let answer = 0;
     answer = n;
     const arr = [];
     const item = answer.toString();
     for (let i = 0; i < item.length; i++) {
-        console.log(item[i]);
         arr.push(item[i]);
     }
     answer = parseInt(arr.sort().reverse().join(""));
-
-    console.log(answer);
     return answer;
 }
 solution4(118372);
+// function solution(n) {
+//     let result = n.toString().split("").sort().reverse().join("");
+//         return parseInt(result);
+//     }
+
+// 자연수 뒤집어 배열로 만들기
+function solution5(n) {
+    const item = n.toString().split("").reverse();
+    const answer = [];
+    for (let i = 0; i < item.length; i++) {
+        answer[i] = parseInt(item[i]);
+    }
+    return answer;
+}
+solution5(12345);
+
+// 제일 작은수 제거
+function solution6(arr) {
+    const min = Math.min(...arr);
+    const index = arr.findIndex((item) => item === min);
+    arr.splice(index, 1);
+
+    if (arr.length > 0) {
+        return arr;
+    }
+    return [-1];
+}
+solution6([4, 3, 2, 1, 1, 2]);
