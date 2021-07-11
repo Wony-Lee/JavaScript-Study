71fa501452060afd34247912c3416635bf08ed7b
Wony-Lee <a1_11_11@naver.com>
2021-07-11 14:17:38 +0900

    숫자 문자열과 영단어

--- al/test.js
+++ al/test.js
@@ -54,3 +54,19 @@ function solution(numbers, hand) {
     return answer;
 }
 solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left");
+
+// 프로그래머스 숫자 문자열과 영단어
+function solution(s) {
+    s = s.replace(/zero/g, "0");
+    s = s.replace(/one/g, "1");
+    s = s.replace(/two/g, "2");
+    s = s.replace(/three/g, "3");
+    s = s.replace(/four/g, "4");
+    s = s.replace(/five/g, "5");
+    s = s.replace(/six/g, "6");
+    s = s.replace(/seven/g, "7");
+    s = s.replace(/eight/g, "8");
+    s = s.replace(/nine/g, "9");
+    return parseInt(s);
+}
+solution("one4seveneight");
